a7a7711e81c7df18895499d0b7582dced38bd292
ThiagoKwon <thiago.kwon.dev@gmail.com>
2025-09-21 17:49:13 -0300

    commit template 1 e 2

--- DesignPatterns/template_Processamento_Relatorio.java
+++ DesignPatterns/template_Processamento_Relatorio.java
@@ -1,4 +1,17 @@
 package EzPet.DesignPatterns;
 
-public class template_Processamento_Relatorio {
+public abstract class template_Processamento_Relatorio {
+
+    // Template Method
+    public final void gerarRelatorio() {
+        coletarDados();      // Passo 1
+        formatar();          // Passo 2
+        // outros passos: enviarOuSalvar(), registrarLog()...
+    }
+
+    // Passo 1 – Coletar dados
+    protected abstract void coletarDados();
+
+    // Passo 2 – Formatar dados
+    protected abstract void formatar();
 }
--- DesignPatterns/template_agendamento.java
+++ DesignPatterns/template_agendamento.java
@@ -1,4 +1,19 @@
 package EzPet.DesignPatterns;
 
-public class template_agendamento {
+public abstract class template_agendamento {
+
+    // Template Method
+    public final void processoAgendamento() {
+        if(verificarDisponibilidade()) {     // Passo 1
+            double valor = calcularValor();  // Passo 2
+        } else {
+            System.out.println("Horário indisponível.");
+        }
+    }
+
+    // Passo 1 – Verificar disponibilidade
+    protected abstract boolean verificarDisponibilidade();
+
+    // Passo 2 – Calcular valor
+    protected abstract double calcularValor();
 }
